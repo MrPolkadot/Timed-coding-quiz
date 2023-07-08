7b6d4126e3c3defc81adc2d0e218306752dd0525
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-07-08 13:52:31 -0600

    Add message display if either right or wrong answer is chosen.

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ useful links:
 https://palettes.shecodes.io/
 https://fonts.google.com/
 https://bobbyhadz.com/blog/javascript-get-element-by-id-contains
-https://www.javatpoint.com/
+https://www.javatpoint.com/(for the questions and choices)
+https://stackoverflow.com/questions/26944987/show-next-previous-item-of-an-array
--- assets/script.js
+++ assets/script.js
@@ -4,52 +4,98 @@ let seconds;
 let timer = document.querySelector("#timer");
 let startingPoint = document.querySelector("#starting-point");
 let startQuizButton = document.querySelector("#start-quiz-button");
+let quizBlock = document.querySelector("#quiz-block");
+let resultText = document.querySelector("#result-text");
+let rightChoice = document.querySelectorAll("#right");
+let wrongChoice = document.querySelectorAll("#wrong");
+
+// let questionOne = document.querySelector("#question-one");
+// let questionTwo = document.querySelector("#question-two");
+// let questionThree = document.querySelector("#question-three");
+// let questionFour = document.querySelector("#question-five");
+// let questionFive = document.querySelector("#question-six");
+// let questionSix = document.querySelector("#question-seven");
+// let questionSeven = document.querySelector("#question-seven");
+// let questionEight = document.querySelector("#question-eight");
+// let questionNine = document.querySelector("#question-nine");
+// let questionTen = document.querySelector("#question-ten");
 let questions = document.querySelectorAll(`[id^="question"]`);
-let rightAnswer = document.querySelectorAll("#right");
-let wrongAnswer = document.querySelectorAll("#wrong");
+
+console.log(wrongChoice);
+
+//Will start the quiz and the countdown
+startQuizButton.addEventListener("click", function () {
+    questions[0].style.visibility = "visible";
+    startingPoint.style.visibility = "hidden";
+    countdown();
+});
+
+//Converts these NodeLists into an array
+let rightChoiceArr = Array.from(rightChoice);
+let wrongChoiceArr = Array.from(wrongChoice);
+let questionsArr = Array.from(questions);
 console.log(questions);
-console.log(rightAnswer);
 
 
-//Work on this
-function wrongChoice() {
-    for (let i = 0; i < wrongAnswer.length; i++) {
-        wrongAnswer[i].addEventListener("click", (e));
-    }
+//Displays message if either right or wrong
+for (let i = 0; i < rightChoiceArr.length; i++) {
+    rightChoiceArr[i].addEventListener("click", function() {
+        displayCorrectMessage();
+        sectionChange();
+    });
 }
 
-startQuizButton.addEventListener("click", countdown);
-startQuizButton.addEventListener("click", sectionChange) //Function that will start the quiz/change section.
-
+for (let i = 0; i < wrongChoiceArr.length; i++) {
+    wrongChoiceArr[i].addEventListener("click", displayWrongMessage);
+    
+};
 
 //Counts down the timer once quiz is started.
 function countdown() {
-    seconds = 10; //Change it to 60 in the future.
+    seconds = 60; //Change it to 60 in the future.
     let timeInterval = setInterval(function () {
         //subtract 3 seconds each time wrong is chosen.
         //if (wrongAnswer) {
-            //seconds = seconds - 3;
+        //seconds = seconds - 3;
         //}
         if (seconds > 0) {
             timer.textContent = seconds;
             seconds--;
-        }  else {
+        } else {
             timer.textContent = "0";
             clearInterval(timeInterval);
             //Enter function here that will stop the game and send you to endpoint section.
         }
     }, 1000);
-}
-
-//Work on how to change to each section after clicking right or wrong.
-function sectionChange() {
-    if (startQuizButton) {
-        questions[0].style.visibility = "visible";
-    } 
-    if (wrongAnswer) {
-        questions[1].style.visibility = "visible";
-        wrongChoice();
-    }
 };
 
 
+function displayCorrectMessage() {
+    if (rightChoiceArr) {
+        resultText.textContent = "Correct";
+    }
+};
+
+function displayWrongMessage() {
+    if (wrongChoiceArr) {
+    resultText.textContent = "Wrong";
+    }
+};
+
+//Work on this
+function sectionChange() {
+    i = 0;
+    i = i + 1;
+    i = i % questionsArr.length;
+    return questionsArr[i]; 
+    
+    // for (let i = 0; i < questionsArr.length; i++) {
+        // return questionsArr[i];
+     }
+    //     if (rightChoiceArr || wrongChoiceArr) {
+    //         questionsArr[1].style.visibility = "visible";
+    //         console.log(questionsArr[i]);
+    //     }
+    // }
+    
+};
