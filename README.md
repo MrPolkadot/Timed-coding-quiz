0ae1c313aff5fdd36e54bb4e46554994d1482610
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-07-13 22:24:37 -0600

    Refactor code. Add table with score and intials in score section with an event listener. Update README file.

--- README.md
+++ README.md
@@ -1,9 +1,72 @@
-# Timed-coding-quiz
+# Timed Coding Quiz
 
-useful links: 
-https://palettes.shecodes.io/
-https://fonts.google.com/
-https://bobbyhadz.com/blog/javascript-get-element-by-id-contains
-https://www.javatpoint.com/(for the questions and choices)
-https://stackoverflow.com/questions/26944987/show-next-previous-item-of-an-array
-https://stackoverflow.com/questions/28535226/javascript-how-to-show-the-next-div-and-hide-the-previous-one
+## Description
+
+The quiz includes 10 questions based on *HTML*, *CSS* and *Javascript*. It includes a timer and keeps track of your score.
+
+This project was useful in making me understand the concepts of DOM manipulation.
+
+
+## Usage
+
+*enter github page link here*
+
+The quiz and timer will start when you click "START QUIZ".
+
+
+![First section of quiz webpage]
+(./assets/images/quiz-start.png)
+
+
+The set of questions will appear. The question will change once you choose an answer.
+
+
+![First question of the quiz with multiple answers.](./assets/images/question-sections.png)
+
+
+The quiz ends after the timer reaches zero or if you answer all the questions. It will show your overall score. Score is based out of 100 max points.
+
+
+![End of the quiz section, showing score results](./assets/images/quiz-end.png)
+
+
+When initials are submitted, the score and initials are added into a table in the score section. 
+
+
+![A table showing a name and score](./assets/images/score-section.png)
+
+
+## Credits
+
+Useful links that helped me complete the project: 
+
+* https://palettes.shecodes.io/
+* https://fonts.google.com/
+* https://bobbyhadz.com/blog/javascript-get-element-by-id-contains
+* https://www.javatpoint.com/*(for the questions and choices)*
+* https://stackoverflow.com/questions/26944987/show-next-previous-item-of-an-array
+* https://stackoverflow.com/questions/28535226/javascript-how-to-show-the-next-div-and-hide-the-previous-one
+
+## License
+
+MIT License
+
+Copyright (c) 2023 Salvador M
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/script.js
+++ assets/script.js
@@ -22,7 +22,6 @@ let questionsArr = [
     document.querySelector("#question-ten"),
     document.querySelector("#end-point")
 ]
-//let questions = document.querySelectorAll(`[id^="question"]`);
 
 let score = document.querySelector("#score");
 let finalScore = 0;
@@ -45,107 +44,107 @@ let choices = rightChoiceArr.concat(wrongChoiceArr);
 
 //Counts down the timer once quiz is started.
 function countdown() {
-    seconds = 10; //Change it to 60 in the future.
+    seconds = 60; //Change it to 60 in the future.
     let timeInterval = setInterval(function () {
         if (seconds <= 0) {
             questionsArr[9].style.visibility = "visible";
+            resultText.style.display = "none";
+            timer.textContent = "";
         }
         if (seconds > 0) {
             timer.textContent = seconds;
             seconds--;
-        } 
+        }
         else {
             timer.textContent = "";
             clearInterval(timeInterval);
         }
+        if (rightChoiceArr[9]) {
+            resultText.textContent = "";
+        }
     }, 1000);
 };
 
-
 //Displays message if either right or wrong
-    for (let i = 0; i < rightChoiceArr.length; i++) {
-        rightChoiceArr[i].addEventListener("click", function() {
-            sectionChange();
-            displayCorrectMessage();
-        });
-    }
+for (let i = 0; i < rightChoiceArr.length; i++) {
+    rightChoiceArr[i].addEventListener("click", function () {
+        sectionChange();
+        if (rightChoiceArr) {
+            finalScore += 10;
+            score.textContent = finalScore;
+            resultText.textContent = "Correct";
+        }
+    });
+}
 
-    for (let i = 0; i < wrongChoiceArr.length; i++) {
-        wrongChoiceArr[i].addEventListener("click", function() {
-            sectionChange();
-            displayWrongMessage();
-        });
-    };
-
-
-
-function displayCorrectMessage() {
-    if (rightChoiceArr) {
-        finalScore += 10;
-        score.textContent = finalScore;
-        
-        resultText.textContent = "Correct";
-    }
-    return finalScore;
+for (let i = 0; i < wrongChoiceArr.length; i++) {
+    wrongChoiceArr[i].addEventListener("click", function () {
+        sectionChange();
+        if (wrongChoiceArr) {
+            seconds -= 5;
+            resultText.textContent = "Wrong";
+        }
+    });
 };
 
-function displayWrongMessage() {
-    if (wrongChoiceArr) {
-        seconds -= 5;
-    resultText.textContent = "Wrong";
-    }
-};
 
 //Changes the visibility of each section
 function sectionChange() {
     for (let i = 0; i < questionsArr.length; i++) {
-        
+
         if (questionsArr[i].style.visibility != "visible") {
             questionsArr[i].style.visibility = "visible";
-            if (i == questionsArr.length) {
-                questionsArr[0].style.visibility = "visible";
-            }
-        break
+            break
         }
+
     }
+
 }
 
-
 //Grabs the elements for the last two sections of the page
-let userInitials = document.querySelector("#initials-input");
-let submit = document.querySelector("#submit");
+let userInitials = document.querySelector("#initials");
+let submitBtn = document.querySelector("#submit-initials");
 let retry = document.querySelector("#retry");
 let scoreBoard = document.querySelector("#score-board");
 let scoreSection = document.querySelector("#score-section");
 let newUserInput = document.querySelector("#new-user");
+let retakeBtn = document.querySelector("#retake");
+let infoForm = document.querySelector("#info-form");
 
 
-
-
-// localStorage.setItem("user", initialsValue);
-// localStorage.setItem("score", finalScore);
-
-function saveInputValue() {
-localStorage.setItem("score", finalScore);
-localStorage.setItem("user", initialsValue);
-};
-
-
-//when clicked, save initials into localstorage and add them into the table.
-submit.addEventListener("click", function() {
-    let initialsValue = userInitials.value;
-    if (initialsValue) {
-        let tableData = document.createElement("tr");
-        newUserInput.appendChild(tableData);
-    }
-    
-    
-    saveInputValue();
+//When initials are submitted, a table is created in the score section.
+submitBtn.addEventListener("click", function (event) {
+    let row1 = document.createElement("tr");
+    let head1 = document.createElement("th");
+    let head2 = document.createElement("th");
+    let row2 = document.createElement("tr");
+    let c1 = document.createElement("td");
+    let c2 = document.createElement("td");
+    head1.textContent = "Name";
+    head2.textContent = "Score";
+    c1.textContent = userInitials.value;
+    c2.textContent = finalScore;
+    row1.appendChild(head1);
+    row1.appendChild(head2);
+    row2.appendChild(c1);
+    row2.appendChild(c2);
+    newUserInput.appendChild(row1);
+    newUserInput.appendChild(row2);
+    event.preventDefault();
+    console.log(newUserInput);
 });
 
 
-scoreBoard.addEventListener("click", function() {
+scoreBoard.addEventListener("click", function (event) {
+    event.preventDefault();
     scoreSection.style.visibility = "visible";
 })
 
-//make function that goes to score-section after clicking check scores
+//These 2 buttons will return to the starting point of the quiz.
+retry.addEventListener("click", function () {
+    location.reload();
+});
+
+retakeBtn.addEventListener("click", function () {
+    location.reload();
+});
